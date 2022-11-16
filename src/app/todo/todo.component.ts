dc719340035c21ef9900125a3ee0b96569b0ce38
zachhudy <zachhudy@gmail.com>
2022-11-15 22:34:41 -0500

    Add update button for use with edit icon; add updateTask method

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -14,7 +14,7 @@ export class TodoComponent implements OnInit {
   tasks: TodoTask[] = [];
   inprogress: TodoTask[] = [];
   done: TodoTask[] = [];
-  updateId!: any;
+  updateIndex!: any;
   isEditEnabled: boolean = false;
 
   constructor(private fb: FormBuilder) { }
@@ -29,13 +29,22 @@ export class TodoComponent implements OnInit {
     this.tasks.push({
       description: this.todoForm.value.item,
       done: false
-    })
+    });
+    this.todoForm.reset();
   }
 
   onEdit(item: TodoTask, i: number) {
     this.todoForm.controls['item'].setValue(item.description);
-    this.updateId = i;
+    this.updateIndex = i;
     this.isEditEnabled = true;
+  }
+
+  updateTask() {
+    this.tasks[this.updateIndex].description = this.todoForm.value.item;
+    this.tasks[this.updateIndex].done = false;
+    this.todoForm.reset();
+    this.updateIndex = undefined;
+    this.isEditEnabled = false;
   }
 
   deleteTask(i: number) {
